8b1c097a72a65b0bff935aa368c12551c1803bf8
gmand <gmand@DESKTOP-2KO8PEB>
2025-06-18 18:27:23 -0400

    Configuração inicial do formulário, URL da API e componente DataGridView

--- WinFormsWebApi/Main.cs
+++ WinFormsWebApi/Main.cs
@@ -2,9 +2,17 @@ namespace WinFormsWebApi
 {
     public partial class Main : Form
     {
+        private readonly ApiClient _apiClient;
+        private BindingSource _produtosBindingSource = new BindingSource();
         public Main()
         {
             InitializeComponent();
+            //Url da API
+            _apiClient = new ApiClient("https://localhost:5001");
+
+            // Configuração do componente DataGridView
+            dataGridView1.AutoGenerateColumns = true;
+            dataGridView1.DataSource = _produtosBindingSource;
         }
     }
 }
